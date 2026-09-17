55ea1c222ba29effebe491183763ee80ce0100f7
Szabi2098 <nagysz@kkszki.hu>
2026-09-17 13:04:39 +0200

    Main

--- Okosotthon/Program.cs
+++ Okosotthon/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Okosotthon.Models;
 
 namespace Okosotthon
 {
     internal class Program
     {
+        private static List<Ertesites> ertesites = new List<Ertesites>();
         static void Main(string[] args)
         {
+            ertesites.Add(new SMS());
+            ertesites.Add(new Email());
+            ertesites.Add(new Push());
+            foreach (var item in ertesites)
+            {
+                item.Kuld("Mozgás érzékelve: Nappali Jobb sarok pókháló");
+            }
         }
     }
 }
